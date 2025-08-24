f2ca78cd7da97a28f1582a979be628b4484d3ed4
Mohamed Hazem <darkg5341@gmail.com>
2025-08-24 08:51:17 +0300

    new

--- public/main.js
+++ public/main.js
@@ -197,27 +197,3 @@ function handleError(error, fallbackMessage = 'An error occurred') {
 function handleSuccess(message) {
     showNotification(message, 'success');
 }
-
-///////////////////////////////////////
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDDJU-m5RcBowWZfwDsbpXKgMmdh2M2WRY",
-  authDomain: "blogy-go.firebaseapp.com",
-  projectId: "blogy-go",
-  storageBucket: "blogy-go.firebasestorage.app",
-  messagingSenderId: "757856471013",
-  appId: "1:757856471013:web:26df956e7ef1814642910c",
-  measurementId: "G-0R45Q2ZZRH"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
